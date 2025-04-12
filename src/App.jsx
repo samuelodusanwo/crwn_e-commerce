643507c78e6b5894b6d5870b38263af3e5188566
youngsal <waleola805@gmail.com>
2025-04-12 18:04:14 +0100

    created project with vite and added files for homepage

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomePage from './homepage/homepage.component'
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <HomePage/>
+    </>
+  )
+}
+
+export default App
