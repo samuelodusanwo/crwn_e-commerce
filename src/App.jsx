bc62550e79d9a2b0e41e62599da23da97ef8ddb7
youngsal <waleola805@gmail.com>
2025-04-14 17:26:05 +0100

    implemented form-input page, sign-in page, input and custom-button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom'
-import Header from './components/header/header.component'
-import HomePage from './pages/homepage/homepage.component'
-import ShopPage from './pages/shop/shop.component'
+import { Routes, Route, Link } from 'react-router-dom';
+import Header from './components/header/header.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 import './App.scss'
 
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path='/' element={ <HomePage /> } />
         <Route path='/shop' element={ <ShopPage /> } />
+        <Route path='/sign-in-and-sign-up' element={ <SignInAndSignUpPage /> } />
       </Routes>
     </>
   )
